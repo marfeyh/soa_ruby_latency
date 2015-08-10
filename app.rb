e1e3eb18fb6d5318e2f948dd9500db518f894682
Markus Feyh <markus.feyh@gmail.com>
2015-08-10 14:28:11 +0200

    Add sleep 5 seconds:

--- app.rb
+++ app.rb
@@ -5,8 +5,9 @@ class HTTPAuthApp < Sinatra::Base
   use Rack::Auth::Basic, "Restricted Area" do |username, password|
     username == 'Alladin' and password == 'password'
   end
-
+  
   get '/' do
+    sleep(5.seconds)
     "Hello, Alladin!"
   end
 
